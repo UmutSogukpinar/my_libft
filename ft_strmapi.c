7bf70308dc0d55acff5a5593d93771e8e9b56585
UmutSogukpinar <usogukpinar0@gmail.com>
2024-10-25 00:20:25 +0300

    update

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,27 +1,4 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: usogukpi <usogukpi@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/15 14:28:27 by usogukpi          #+#    #+#             */
-/*   Updated: 2024/10/15 14:30:02 by usogukpi         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
-
-char toUpper(unsigned int i, char c)
-{
-	if (c >= 97 && c <= 122)
-	{
-		c -= 32;
-		return (c);
-	}
-	else
-		return (c);
-}
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
